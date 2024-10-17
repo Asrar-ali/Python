f3f945e7a0dc821f1f3a8b983346d84e738411a6
Asrar-ali <mohammedasrarali99@gmail.com>
2024-10-16 23:15:05 -0400

    Update class_definition.py

--- Classes and objects/Definition/class_definition.py
+++ Classes and objects/Definition/class_definition.py
@@ -7,7 +7,7 @@ class MyClass:
 
 # `my_object` holds an object of the class "MyClass" that contains
 # the `variable` and the `foo` function
-my_object = ???
+my_object = Myclass()
 
 my_object.foo()  # Calls the `foo` method defined in MyClass
 print(my_object.variable)  # Prints the value of the `variable` attribute defined in MyClass
