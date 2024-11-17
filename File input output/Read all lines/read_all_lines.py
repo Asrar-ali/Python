01fdcd1b0c433894beb5199533d050ccc7d71c5c
Asrar-ali <mohammedasrarali99@gmail.com>
2024-11-16 22:25:17 -0500

    Update read_all_lines.py

--- File input output/Read all lines/read_all_lines.py
+++ File input output/Read all lines/read_all_lines.py
@@ -1,5 +1,5 @@
 with open("input.txt", "r") as infile:
-    # TODO: read all lines from input.txt into the list called lines_list
+    # TODO: read all lines from input.txt into the list called lines_line
 
 if __name__ == "__main__":
     print(lines_list)
